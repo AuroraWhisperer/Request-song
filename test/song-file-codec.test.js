511df7db8e80965157eab2436d7177e9cda255ef
AuroraWhisperer <137684179+AuroraWhisperer@users.noreply.github.com>
2026-08-04 15:58:08 +0800

    v3.0.12

--- test/song-file-codec.test.js
+++ test/song-file-codec.test.js
@@ -11,7 +11,19 @@ const {
   SONG_IMPORT_ALIASES,
   normalizeImportedSongRow
 } = require('../src/music/song-import-schema');
+const { createZip, readZipFiles } = require('../src/shared/utils');
 const songService = require('../src/music/song-service');
+
+function namespaceWorksheetTags(buffer) {
+  const files = readZipFiles(buffer);
+  const worksheetPath = 'xl/worksheets/sheet1.xml';
+  const worksheet = files.get(worksheetPath)
+    .replace(/<worksheet xmlns="([^"]+)"/, '<worksheet xmlns="$1" xmlns:x="$1"')
+    .replace(/t="inlineStr"><is><t>([\s\S]*?)<\/t><\/is>/g, 't="str"><v>$1</v>')
+    .replace(/<(\/?)(row|c|is|t|v)(\b)/g, '<$1x:$2$3');
+  files.set(worksheetPath, worksheet);
+  return createZip(Array.from(files.entries()));
+}
 
 test('song workbook codec preserves export fields for the import schema', () => {
   const songs = [{
@@ -42,4 +54,17 @@ test('song workbook codec preserves export fields for the import schema', () => 
 test('song service keeps its import schema compatibility exports', () => {
   assert.equal(songService.SONG_IMPORT_ALIASES, SONG_IMPORT_ALIASES);
   assert.equal(songService.normalizeImportedSongRow, normalizeImportedSongRow);
+});
+
+test('song workbook codec parses namespace-prefixed worksheet tags', () => {
+  const workbook = buildSongsWorkbook([{
+    name: 'Namespaced song',
+    artist: 'Test artist',
+    category_name: 'Test category',
+    is_enabled: true
+  }]);
+
+  const [row] = parseSongsFromXlsx(namespaceWorksheetTags(workbook));
+
+  assert.equal(normalizeImportedSongRow(row).name, 'Namespaced song');
 });
